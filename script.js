720c5f687bbe4a21b706f434bef08f637e95317b
527esilvia-sudo <527esilvia@frhsd.com>
2026-01-10 19:06:13 +0000

    Half of first switch-case done

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ function getPrefix(food) {
     switch(food) {
     case 'McDonalds':
         return 'Peasant';
-    case 'Burger King':
-     return 'Commoner';
+    case 'Chick-fil-a':
+        return 'Farmer';
     case 'Wendys':
-        return 'Noble';
-    case 'Chick-fil-A':
-        return 'Royalty';
-    case 'Panera Bread':
-        return 'Peasant';
-          case 'Popeyes':
-        return 'Peasant';
+        return 'Guard';
+    case 'Burger King':
+        return 'Knight';
     case 'Taco Bell':
-     return 'Commoner';
-    case 'Shake Shack':
         return 'Noble';
-    case 'Raising Canes':
-        return 'Royalty';
-    case 'Dairy Queen':
-        return 'Peasant';
+    case 'Subway':
+        return 'Baron';
+        case 'KFC':
+        return 'Commoner';
+    case 'Popeyes':
+        return 'Count';
+    case 'In-N-Out':
+        return 'Squire';
+    case 'Five Guys':
+        return 'Lord';
+    case 'Arbys':
+        return 'Royal';
+    case 'Jack In The Box':
+        return 'Soldier ';
+    
 
     default:
         return 'Peasant';   
